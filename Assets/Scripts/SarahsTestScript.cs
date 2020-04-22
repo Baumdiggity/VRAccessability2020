b7099e0362c6c6d07064b3282b155f51e140c53c
sarahCRichmond <sarahcrichmond@gmail.com>
2020-04-22 15:56:56 -0500

    Added sarah test scene and script :) testing commit
    Added sarah test scene and script :) testing commit

---
+++ Assets/Scripts/SarahsTestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SarahsTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("yo~!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
