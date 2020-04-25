f976c83a588946e420fda354deaa35b19ea6ae24
sarahcrichmond <sarah.richmond@medcognition.com>
2020-04-25 14:24:34 -0500

    Made some changes to tut scene, placed new robot.
    Made some changes to tut scene, placed new robot.

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class CameraSwitcher : MonoBehaviour
 {
@@ -15,12 +16,23 @@ public class CameraSwitcher : MonoBehaviour
         listOfCameras = new List<GameObject>(GameObject.FindGameObjectsWithTag("TeleportCamera"));
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwapCamera();
+        }
+    }
+
     public void SwapCamera()
     {
         currentIndx++;
-        if (currentIndx > listOfCameras.Count)
+        if (currentIndx > listOfCameras.Count -1)
         {
             currentIndx = 0;
         }
+
+        //teleport player here
+        Teleport.instance.TeleportPlayerToPoint(listOfCameras[currentIndx].gameObject.transform);
     }
 }
