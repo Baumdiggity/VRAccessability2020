786f70b1e553537585635807f3523a8db69a1693
Regretful123 <jordbej@yahoo.com>
2020-04-24 23:13:14 -0500

    Implementing PhysicsTrigger
    Added PhysicsTrigger to help ease the level design scripting. This script allows us to implement behavior based on the trigger/collision the object gives. - JB

---
+++ Assets/PhysicsTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[Serializable]
+public class TriggerEvent : UnityEvent<object> { }
+
+[Serializable]
+public class CollisionEvent : UnityEvent<object> { }
+
+public class PhysicsTrigger : MonoBehaviour
+{
+    public string filterBy;
+    public TriggerEvent TriggerEnter;
+    public TriggerEvent TriggerStay;
+    public TriggerEvent TriggerLeave;
+
+    public CollisionEvent CollisionEnter;
+    public CollisionEvent CollisionStay;
+    public CollisionEvent CollisionExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if( string.IsNullOrEmpty(filterBy) || other.tag == filterBy )
+            TriggerEnter.Invoke(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (string.IsNullOrEmpty(filterBy) || other.tag == filterBy)
+            TriggerStay.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (string.IsNullOrEmpty(filterBy) || other.tag == filterBy)
+            TriggerLeave.Invoke(other);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (string.IsNullOrEmpty(filterBy) || collision.gameObject.tag == filterBy)
+            CollisionEnter.Invoke(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (string.IsNullOrEmpty(filterBy) || collision.gameObject.tag == filterBy)
+            CollisionStay.Invoke(collision);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (string.IsNullOrEmpty(filterBy) || collision.gameObject.tag == filterBy)
+            CollisionExit.Invoke(collision);
+    }
+
+}
