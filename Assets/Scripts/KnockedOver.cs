8064f4272e7af80cfa37f2773359e7facfdc1ac2
Wander <alicia.anne.baum@gmail.com>
2020-04-25 16:17:33 -0700

    Trash Cans and bags produce trash

---
+++ Assets/Scripts/KnockedOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockedOver : MonoBehaviour
+{
+    public GameObject[] trashObjects;
+    public GameObject dustPoof;
+    bool exploding = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.up.y < .6f && !exploding)
+        {
+            exploding = true;
+            dustPoof.SetActive(true);
+            for (int i = Random.Range(1,5); i > 0; i--)
+            {
+
+                GameObject gO = Instantiate(trashObjects[Random.Range(0, trashObjects.Length - 1)], transform.position + Vector3.up, transform.rotation);
+                if (gO.GetComponent<Rigidbody>() != null)
+                {
+                    gO.GetComponent<Rigidbody>().AddExplosionForce(0.25f, this.transform.position, 1f);
+                    Debug.Log("Exploded");
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/TrashDump.cs
+++ Assets/Scripts/TrashDump.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TrashDump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] trashObjects;
+    public GameObject dustPoof;
 
-    // Update is called once per frame
-    void Update()
+    bool exploding = false;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.CompareTag("Robot") && !exploding)
+        {
+            exploding = true;
+            dustPoof.SetActive(true);
+            for(int i =5; i>0; i--)
+            {
+
+               GameObject gO = Instantiate(trashObjects[Random.Range(0, trashObjects.Length - 1)], transform.position + Vector3.up, transform.rotation);
+                if(gO.GetComponent<Rigidbody>() != null)
+                {
+                    gO.GetComponent<Rigidbody>().AddExplosionForce(.5f, this.transform.position, 1f);
+                    Debug.Log("Exploded");
+                }
+            }
+            this.gameObject.SetActive(false);
+            
+        }
     }
 }
