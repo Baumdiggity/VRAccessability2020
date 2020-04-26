dc69bfc4bd2271981d53d09dbbeca0cc783d3543
sarahcrichmond <sarah.richmond@medcognition.com>
2020-04-26 15:10:47 -0500

    Made final win scene
    Made final win scene

---
+++ Assets/loadMenuScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadMenuScene : MonoBehaviour
+{
+    private float countDown = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+            SceneManager.LoadScene("TutorialLevel");
+
+    }
+}
