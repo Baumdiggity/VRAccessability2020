d40f8b086970f211f920f947c7f6f27b6840f888
Wander <alicia.anne.baum@gmail.com>
2020-04-25 22:17:35 -0700

    Added Touchable Sink
    Clean your grubby mitts with the touch of a anything

---
+++ Assets/Scripts/SinkTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkTrigger : MonoBehaviour
+{
+    public GameObject particleObject;
+    bool isON = false;
+    bool delay = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Robot"))
+        {
+            if (!delay)
+            {
+                isON = !isON;
+                particleObject.SetActive(isON);
+                StartCoroutine("SinkOn");
+            }
+        }
+    }
+
+    IEnumerator SinkOn()
+    {
+        delay = true;
+        yield return new WaitForSeconds(1f);
+        delay = false;
+    }
+}
