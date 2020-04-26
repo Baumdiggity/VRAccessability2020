160d3f415646062167d0cdc57f4651110ce82963
Regretful123 <jordbej@yahoo.com>
2020-04-25 21:59:26 -0500

    Update level 2
    Update level 2 to refine testing

--- Assets/Scripts/WinLevel.cs
+++ Assets/Scripts/WinLevel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLevel : MonoBehaviour
 {
+    public string NextLevel;
+    [Range(0.0f, 30f)]
+    public float waitTimeToLoadNextScene = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,12 @@ public class WinLevel : MonoBehaviour
     public void Win()
     {
         Debug.Log("Successfully passed the butter, you win!");
+        // should we load the scene???
+        Invoke("LoadScene", waitTimeToLoadNextScene);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(NextLevel);
     }
 }
